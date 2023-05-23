bd44aa2319322c543c02dc90bcd514fe48b97bd1
Nived K S <chippukichu10@gmail.com>
2023-05-23 12:09:55 +0530

    Component folder created and added files

---
+++ src/Components/Screen.js
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { CalcContext } from "../context/CalcContext"
+
+
+const Screen = () => {
+    const{calc}=useContext(CalcContext);
+  return (
+    <div className="screen">
+      07986887
+    </div>
+  )
+}
+
+export default Screen
